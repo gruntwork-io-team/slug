e22b11a441586d2aa6224feeec2396430a1f23b7
Zach Goldberg <zach@zachgoldberg.com>
2024-12-11 10:43:04 -0800

    use the neon adapter

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { env } from "@/env.mjs";
-import { Pool } from 'pg'
-import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaNeon } from '@prisma/adapter-neon'
+import { Pool } from '@neondatabase/serverless'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
-const adapter = new PrismaPg(pool)
+const adapter = new PrismaNeon(pool)
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
