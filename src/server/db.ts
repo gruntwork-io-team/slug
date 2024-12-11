0b9ae97c2204f83bff8395e03db8976de004608a
Zach Goldberg <zach@zachgoldberg.com>
2024-12-11 09:18:52 -0800

    switch off turso

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,23 +1,14 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 import { env } from "@/env.mjs";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const libsql = createClient({
-  url: env.TURSO_DATABASE_URL ?? "",
-  authToken: env.TURSO_AUTH_TOKEN ?? "",
-});
-
-const adapter = new PrismaLibSQL(libsql);
 
 export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
